a32ef85d374085b9e520dd7d7fd6b9c0390151be
tatiana <tatiana.tokmantseva@gmail.com>
2022-11-12 18:00:42 +0500

    Третий коммит

--- src/Main.java
+++ src/Main.java
@@ -11,43 +11,60 @@ public class Main {
 //3. Произвольный массив – тип и количество данных определите сами. Самостоятельно выберите способ создания
 // массива: с помощью ключевого слова или сразу заполненный элементами.
 
-        int[] nums = new int[3];
-        nums[0] = 1;
-        nums[1] = 2;
-        nums[2] = 3;
-        System.out.print(nums[0] +", ");
-        System.out.print(nums[1] +", ");
-        System.out.print(nums[2] +"  ");
+        int[] nums = {1, 2, 3};
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i]);
+            if (i != nums.length - 1) {
+                System.out.print(", ");
+            }
+        }
         System.out.println(" ");
-        System.out.print(nums[2] + ", ");
-        System.out.print(nums[1] + ", ");
-        System.out.print(nums[0]);
 
+        int[] numsReverse = {1, 2, 3};
+        for (int i = numsReverse.length - 1; i >= 0; i--) {
+            System.out.print(numsReverse[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
         System.out.println(" ");
         System.out.println(" ");
 
         double[] decimal = {1.57, 7.654, 9.986};
         for (int i = 0; i < decimal.length; i++) {
-           System.out.print(decimal[i] + ", ");//Так и не смогла найти способ, как вывести данные через запятую, при том, чтоб в конке запятой не было
+           System.out.print(decimal[i]);
+           if (i != decimal.length - 1) {
+               System.out.print(", ");
+           }
         }
         System.out.println(" ");
         double[] decimalReverse = {1.57, 7.654, 9.986};
         for (int i = decimalReverse.length - 1; i >= 0; i--) {
-            System.out.print(decimal[i] + ", ");
+            System.out.print(decimal[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
         }
         System.out.println(" ");
         System.out.println(" ");
 
         String[] catsNames = {"Васька", "Кузя", "Барсик", "Мурзик", "Леопольд"};
         for (int i = 0; i < catsNames.length; i++) {
-            System.out.print(catsNames[i] + ", ");
+            System.out.print(catsNames[i]);
+            if (i != catsNames.length - 1) {
+                System.out.print(", ");
+            }
         }
         System.out.println(" ");
 
         String[] catsNamesReverse = {"Васька", "Кузя", "Барсик", "Мурзик", "Леопольд"};
         for (int i = catsNamesReverse.length - 1; i >=0; i--) {
-            System.out.print(catsNamesReverse[i] + ", ");
+            System.out.print(catsNamesReverse[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
         }
+        System.out.println(" ");
         System.out.println(" ");
 
 //4 задача
@@ -55,14 +72,12 @@ public class Main {
 //Важно: код должен работать с любым целочисленным массивом, поэтому для решения задания вам нужно использовать
 // циклы. Распечатайте результат преобразования в консоль.
 
-        int[] number = new int[3];
-        number[0] = 1;
-        number[1] = 2;
-        number[2] = 3;
+        int[] number = {1, 2, 3};
         for (int i = 0; i < number.length; i++) {
-            if (i % 2 == 0) {
-                System.out.println(number[i]); //Не понимаю, куда записать +1, чтоб получчились четные вместо нечетных
+            if (number[i] % 2 != 0) {
+                number[i] +=1;
             }
         }
+        System.out.println(Arrays.toString( number));
     }
 }
