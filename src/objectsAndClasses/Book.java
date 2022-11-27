9a11f31c48f8eaf8ed4770d60b8896905add47d9
tatiana <tatiana.tokmantseva@gmail.com>
2022-11-27 18:54:27 +0500

    ДЗ объекты и классы

---
+++ src/objectsAndClasses/Book.java
@@ -0,0 +1,34 @@
+package objectsAndClasses;
+
+public class Book {
+    private String titleBook;
+    private Author author;
+    private int date;
+
+    public Book(String titleBook, Author author, int date) {
+        this.titleBook = titleBook;
+        this.author = author;
+        this.date = date;
+    }
+
+    public String getTitleBook() {
+        return this.titleBook;
+    }
+
+    public Author getAuthor() {
+        return this.author;
+    }
+
+    public int getDate() {
+        return this.date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public String toString() {
+        return "Книга: " + this.titleBook + "; Автор: " + this.author + "; Дата публикации: " + this.date;
+    }
+}
+
---
+++ src/objectsAndClasses/MainObjectsAndClasses.java
@@ -0,0 +1,21 @@
+package objectsAndClasses;
+
+import java.io.PrintStream;
+
+public class MainObjectsAndClasses {
+    public MainObjectsAndClasses() {
+    }
+
+    public static void main(String[] args) {
+        Author author1 = new Author("Александр", "Пушкин");
+        Author author2 = new Author("Владислав", "Крапивин");
+
+        Book book1 = new Book("Сказка о царе Салтане", author1, 1831);
+        Book book2 = new Book("Дети синего фламинго", author2, 1981);
+
+        book1.setDate(1831);
+
+        System.out.println(book1);
+        System.out.println(book2);
+    }
+}
