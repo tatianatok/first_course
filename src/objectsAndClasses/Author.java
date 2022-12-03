a92a083728485cf44ad9277a7167cdd8f30082e9
tatiana <tatiana.tokmantseva@gmail.com>
2022-12-03 14:25:58 +0500

    ДЗ объекты и классы

--- src/objectsAndClasses/Author.java
+++ src/objectsAndClasses/Author.java
@@ -1,24 +1,29 @@
+
 package objectsAndClasses;
+
+import java.util.Objects;
 
 public class Author {
     private String name;
     private String surname;
 
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(name, author.name) && Objects.equals(surname, author.surname);
+    }
+
+    public int hashCode() {
+        return Objects.hash(name, surname);
+    }
+
+    public String toString() {
+        return name + " " + surname;
+    }
+
     public Author(String name, String surname) {
         this.name = name;
         this.surname = surname;
     }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public String getSurname() {
-        return this.surname;
-    }
-
-    public String toString(){
-        return this.name + " " + this.surname;
-    }
-
 }
--- src/objectsAndClasses/Book.java
+++ src/objectsAndClasses/Book.java
@@ -1,34 +1,39 @@
 package objectsAndClasses;
 
+import java.util.Objects;
+
 public class Book {
     private String titleBook;
     private Author author;
     private int date;
 
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return date == book.date && Objects.equals(titleBook, book.titleBook) && Objects.equals(author, book.author);
+    }
+
+    public int hashCode() {
+        return Objects.hash(titleBook, author, date);
+    }
     public Book(String titleBook, Author author, int date) {
         this.titleBook = titleBook;
         this.author = author;
         this.date = date;
     }
 
-    public String getTitleBook() {
-        return this.titleBook;
-    }
-
-    public Author getAuthor() {
-        return this.author;
+    public String toString() {
+        return "Книга: " + this.titleBook + "; Автор: " + this.author + "; Дата публикации: " + this.date;
     }
 
     public int getDate() {
         return this.date;
     }
-
     public void setDate(int date) {
         this.date = date;
     }
 
-    public String toString() {
-        return "Книга: " + this.titleBook + "; Автор: " + this.author + "; Дата публикации: " + this.date;
-    }
+
 }
 
